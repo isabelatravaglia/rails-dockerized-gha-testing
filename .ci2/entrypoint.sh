e7d940c6a9c53ec4e37632f304bda8e898ef5259
isabela santos <isabelatravaglia@gmail.com>
2021-05-18 18:18:09 +0000

    added a database.github.yml file with a specific config for the test dabatabase during the testing workflow.

--- .ci2/entrypoint.sh
+++ .ci2/entrypoint.sh
@@ -8,6 +8,10 @@ for file in $PWD/*; do
   echo "${file##*/}"
 done
 
+echo "copying new test db config"
+cp config/database.github.yml config/database.yml
+echo "finished copying new test db config"
+
 echo "running db:prepare"
 bundle exec rails db:prepare
 echo "finished running db:prepare"
